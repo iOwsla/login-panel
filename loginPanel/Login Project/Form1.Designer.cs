97bcb3a9316fed3fad43628488e46c721721e059
Owsla <81816429+iOwsla@users.noreply.github.com>
2023-08-10 21:58:16 +0300

    Proje dosyaları.

---
+++ loginPanel/Login Project/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Login_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            panel2 = new Panel();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            maskedTextBox1 = new MaskedTextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 138);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(166, 25);
+            label1.TabIndex = 0;
+            label1.Text = "TC Kimlik Numara:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(126, 187);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Şifre:";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.IndianRed;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(-7, -30);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(593, 144);
+            panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(402, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(161, 87);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("SimSun", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(26, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(370, 29);
+            label3.TabIndex = 0;
+            label3.Text = "Online Ders Giriş Paneli";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.IndianRed;
+            panel2.ForeColor = Color.Black;
+            panel2.Location = new Point(-7, 305);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(606, 65);
+            panel2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(441, 229);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 40);
+            button1.TabIndex = 6;
+            button1.Text = "Giriş Yap";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(188, 218);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(127, 29);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "Beni hatırla";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(188, 179);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(368, 33);
+            maskedTextBox1.TabIndex = 8;
+            maskedTextBox1.UseSystemPasswordChar = true;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(188, 130);
+            maskedTextBox2.Mask = "00000000000";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(368, 33);
+            maskedTextBox2.TabIndex = 9;
+            maskedTextBox2.ValidatingType = typeof(int);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(578, 364);
+            Controls.Add(maskedTextBox2);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5);
+            Name = "Form1";
+            Text = "Kullanıcı Giriş Paneli";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Button button1;
+        private CheckBox checkBox1;
+        private MaskedTextBox maskedTextBox1;
+        private MaskedTextBox maskedTextBox2;
+    }
+}
---
+++ loginPanel/Login Project/Form1.cs
@@ -0,0 +1,10 @@
+namespace Login_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
